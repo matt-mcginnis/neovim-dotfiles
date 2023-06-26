2eab6f8b620e48fa91b75cd0343308b966abb9dc
Matt McGinnis <matt.mcginnis@mckinney.com>
2023-06-26 10:01:45 -0400

    Breaking change for completions

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -49,13 +49,11 @@ cmp.setup({
             require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
         end,
     },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
+    mapping = {
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        ['<C-n>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-N>'] = cmp_action.luasnip_jump_backward(),
+    },
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'luasnip' }, -- For luasnip users.
