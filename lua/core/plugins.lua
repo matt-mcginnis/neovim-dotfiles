444d453a65fb6a71488f23d1bd1287af6847f247
Matt McGinnis <matt.mcginnis@mckinney.com>
2024-02-08 20:14:12 -0500

    feature: Add Telescope File Browser

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -26,6 +26,14 @@ require("lazy").setup({
         tag = "0.1.5",
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
+    {
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons"
+        }
+    },
 
     -- LSP Plugins
     { "williamboman/mason.nvim" },
@@ -72,16 +80,4 @@ require("lazy").setup({
 
     -- Follow Markdown Links
     { "jghauser/follow-md-links.nvim" },
-
-    -- Neo-tree
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        }
-    }
 })
