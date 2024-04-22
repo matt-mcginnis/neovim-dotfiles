aa6a03ad1b9004cc93c910e60285ded61ddc4281
Matt McGinnis <mcginnisma90@gmail.com>
2024-04-22 08:28:12 -0400

    maintenance: Update movement

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 
-vim.keymap.set({"i", "n", "v", "s"}, "<c-space>", "<esc>")
+vim.keymap.set({ "i", "n", "v", "s" }, "<c-space>", "<esc>")
 
 vim.keymap.set("n", "j", "h")
 vim.keymap.set("n", "k", "j")
@@ -9,34 +9,34 @@ vim.keymap.set("n", "h", "k")
 vim.keymap.set("n", "<leader>,", "_")
 vim.keymap.set("n", "<leader>.", "$")
 
-vim.keymap.set("n", "<c-.>", ":b#<cr>", {desc = "Go to last open buffer"})
+vim.keymap.set("n", "<c-.>", ":b#<cr>", { desc = "Go to last open buffer" })
 
-vim.keymap.set("n", "<leader>ca", "za", {desc = "Toggle collapsable fold"})
-vim.keymap.set("n", "<leader>cr", "zr", {desc = "Open a collapsable fold"})
-vim.keymap.set("n", "<leader>cR", "zR", {desc = "Open all collapsable folds recursively"})
-vim.keymap.set("n", "<leader>cm", "zm", {desc = "Close a collapsable fold"})
-vim.keymap.set("n", "<leader>cM", "zM", {desc = "Close all collapsable folds recursively"})
+vim.keymap.set("n", "<leader>ca", "za", { desc = "Toggle collapsable fold" })
+vim.keymap.set("n", "<leader>cr", "zr", { desc = "Open a collapsable fold" })
+vim.keymap.set("n", "<leader>cR", "zR", { desc = "Open all collapsable folds recursively" })
+vim.keymap.set("n", "<leader>cm", "zm", { desc = "Close a collapsable fold" })
+vim.keymap.set("n", "<leader>cM", "zM", { desc = "Close all collapsable folds recursively" })
 
-vim.keymap.set('n', '<space>ef', function() vim.lsp.buf.format { async = true } end)
-vim.keymap.set("n", "<leader>el",  ":vnew | term open_ollama_model<cr>", {desc = "Toggle vertical Ollama terminal"})
+vim.keymap.set('n', '<leader>ef', function() vim.lsp.buf.format { async = true } end)
+vim.keymap.set("n", "<leader>el", ":vnew | term open_ollama_model<cr>", { desc = "Toggle vertical Ollama terminal" })
 
-vim.keymap.set("n", "H", "<C-u>zz", {desc = "Page down and center"})
-vim.keymap.set("n", "K", "<C-d>zz", {desc = "Page up and center"})
+vim.keymap.set("n", "<leader>H", "<C-u>zz", { desc = "Page down and center" })
+vim.keymap.set("n", "<leader>K", "<C-d>zz", { desc = "Page up and center" })
 
-vim.keymap.set("n", "<leader>h", ":set hlsearch! hlsearch?<cr>", {desc = "Toggle search highlight"})
+vim.keymap.set("n", "<leader>h", ":set hlsearch! hlsearch?<cr>", { desc = "Toggle search highlight" })
 
-vim.keymap.set("n", "<c-m>", "m", {desc = "Set mark"})
-vim.keymap.set("n", "m", "`", {desc = "Go to mark"})
+vim.keymap.set("n", "<c-m>", "m", { desc = "Set mark" })
+vim.keymap.set("n", "m", "`", { desc = "Go to mark" })
 
-vim.keymap.set("n", "n", "nzzzv", {desc = "Move to next in search and center"})
-vim.keymap.set("n", "N", "Nzzzv", {desc = "Move to previous in search and center"})
+vim.keymap.set("n", "n", "nzzzv", { desc = "Move to next in search and center" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Move to previous in search and center" })
 
-vim.keymap.set("n", "<leader>p", "\"+p", {desc = "Paste from computer register"})
+vim.keymap.set("n", "<leader>p", "\"+p", { desc = "Paste from computer register" })
 
-vim.keymap.set("n", "<leader>qo", ":copen<cr>", {desc = "Open quickfix"})
-vim.keymap.set("n", "<leader>qn", ":cn<cr>", {desc = "Move to next quickfix"})
-vim.keymap.set("n", "<leader>qp", ":cp<cr>", {desc = "Move to previous quickfix"})
-vim.keymap.set("n", "<leader>qd", ":cdo ", {desc = "Apply quickfix"})
+vim.keymap.set("n", "<leader>qo", ":copen<cr>", { desc = "Open quickfix" })
+vim.keymap.set("n", "<leader>qn", ":cn<cr>", { desc = "Move to next quickfix" })
+vim.keymap.set("n", "<leader>qp", ":cp<cr>", { desc = "Move to previous quickfix" })
+vim.keymap.set("n", "<leader>qd", ":cdo ", { desc = "Apply quickfix" })
 
 vim.keymap.set("n", "<leader>rj", ":vertical resize -10<cr>")
 vim.keymap.set("n", "<leader>rl", ":vertical resize +10<cr>")
@@ -45,6 +45,9 @@ vim.keymap.set("n", "<leader>rk", ":resize -10<cr>")
 
 vim.keymap.set("n", "<leader>st", ":%s/")
 vim.keymap.set("n", "<leader>s.", "@:")
+
+vim.keymap.set("n", "K", "}")
+vim.keymap.set("n", "H", "{")
 
 vim.keymap.set("n", "<c-j>", "<c-w>h")
 vim.keymap.set("n", "<c-l>", "<c-w>l")
@@ -75,10 +78,11 @@ vim.keymap.set("v", "h", "k")
 vim.keymap.set("v", "<leader>,", "_")
 vim.keymap.set("v", "<leader>.", "$")
 
-vim.keymap.set("v", "H", "<C-u>zz")
-vim.keymap.set("v", "K", "<C-d>zz")
+vim.keymap.set("n", "K", "}")
+vim.keymap.set("n", "H", "{")
 
-vim.keymap.set("v", "<leader>eb", ":!black .<cr><cr>", {desc = "Open Black for Python formatting"})
+vim.keymap.set("v", "<leader>H", "<C-u>zz")
+vim.keymap.set("v", "<leader>K", "<C-d>zz")
 
 vim.keymap.set("v", "p", "\"_dP")
 vim.keymap.set("v", "<leader>p", "\"+p")
