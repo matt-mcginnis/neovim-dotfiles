da9de9a9a2eca1e26517fa54c8c31f35f6f43b7f
Matt McGinnis <mcginnisma90@gmail.com>
2024-04-24 14:12:16 -0400

    maintenance: Update visual movement

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -78,8 +78,8 @@ vim.keymap.set("v", "h", "k")
 vim.keymap.set("v", "<leader>,", "_")
 vim.keymap.set("v", "<leader>.", "$")
 
-vim.keymap.set("n", "K", "}")
-vim.keymap.set("n", "H", "{")
+vim.keymap.set("v", "K", "}")
+vim.keymap.set("v", "H", "{")
 
 vim.keymap.set("v", "<leader>H", "<C-u>zz")
 vim.keymap.set("v", "<leader>K", "<C-d>zz")
