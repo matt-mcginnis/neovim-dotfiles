aa6d2d70e892ad5ff9bdfa87caab652d95fd94c5
Matt McGinnis <mcginnisma90@gmail.com>
2024-07-19 20:07:34 -0400

    feature: Remove telescope

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -71,21 +71,6 @@ require("lazy").setup({
         build = ":TSUpdate",
     },
 
-    -- Telescope
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = "0.1.5",
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {
-        "nvim-telescope/telescope-file-browser.nvim",
-        dependencies = {
-            "nvim-telescope/telescope.nvim",
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons"
-        }
-    },
-
     { "nvim-treesitter/nvim-treesitter-textobjects" },
 
     -- Undotree
