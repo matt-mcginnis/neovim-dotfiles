3283afc8c5659e15dd9bd1eb8028c284bbeb3a16
Matt McGinnis <mcginnisma90@gmail.com>
2024-12-06 09:16:15 -0500

    feature: Add Python DAP back

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -63,6 +63,12 @@ require("lazy").setup({
         }
     },
 
+    -- DAP Plugins
+    { 'mfussenegger/nvim-dap' },
+    { 'mfussenegger/nvim-dap-python',     dependencies = { "mfussenegger/nvim-dap" } },
+    { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} },
+    { "theHamsta/nvim-dap-virtual-text" },
+
     -- Markdown Render
     {
         'MeanderingProgrammer/render-markdown.nvim',
