dd91f37660d36f6ea2a0a5235293d1ba6f3e04b3
Matt McGinnis <mcginnisma90@gmail.com>
2025-01-21 17:23:02 -0500

    feature: Plugin updates and obsidian config change

--- lua/core/plugin_config/obsidian.lua
+++ lua/core/plugin_config/obsidian.lua
@@ -1,12 +1,13 @@
 require("obsidian").setup({
     lazy = true,
+    ft = "markdown",
     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    event = {
-      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-      "BufReadPre" .. vim.fn.expand "~" .. "Github/mckinney/ai-obsidian/**.md",
-      "BufNewFile" .. vim.fn.expand "~" .. "Github/mckinney/ai-obsidian/**.md",
-    },
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    --   "BufReadPre" .. vim.fn.expand "~" .. "Github/mckinney/ai-obsidian/**.md",
+    --   "BufNewFile" .. vim.fn.expand "~" .. "Github/mckinney/ai-obsidian/**.md",
+    -- },
     workspaces = {
         {
             name = "work",
