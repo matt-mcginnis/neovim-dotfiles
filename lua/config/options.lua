fb23d642a1a4d3e771885a4401a33d9579577407
Matt McGinnis <mcginnisma90@gmail.com>
2025-03-15 21:47:10 -0400

    feature: Load telescope find files on startup

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -23,3 +23,12 @@ vim.opt.scrolloff = 8
 vim.diagnostic.config({ signs = false, virtual_text = false })
 
 vim.opt.conceallevel = 1
+
+-- Load telescope find_files on startup
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argv(0) == "" then
+      require("telescope.builtin").find_files()
+    end
+  end,
+})
