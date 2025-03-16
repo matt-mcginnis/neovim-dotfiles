de7cd070d890e66d49b77f300dcaa5959d9ab329
Matt McGinnis <mcginnisma90@gmail.com>
2025-03-15 21:40:36 -0400

    feature: Add Yazi keybind

--- lua/config/toggle-term.lua
+++ lua/config/toggle-term.lua
@@ -11,9 +11,6 @@ require("toggleterm").setup {
     direction = 'horizontal' -- 'vertical' | 'horizontal' | 'tab' | 'float',
 }
 
--- Variable to keep track of the current terminal direction
-local toggleterm_direction = 'horizontal'
-
 function _G.set_terminal_keymaps()
     local opts = { buffer = 0 }
     vim.keymap.set('t', '<C-q>', [[<C-\><C-n>]], opts)
@@ -26,11 +23,15 @@ end
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
-
 -- Lazygit Setup
 local Terminal  = require('toggleterm.terminal').Terminal
 local lazygit   = Terminal:new({ cmd = "lazygit", direction = "tab", hidden = true })
 local mergetool = Terminal:new({ cmd = "git mergetool", direction = "tab", hidden = true })
+local yazi = Terminal:new({ cmd = "yazi", direction = "tab", hidden = true })
+
+function _Yazi_Toggle()
+    yazi:toggle()
+end
 
 function _Lazygit_Toggle()
     lazygit:toggle()
@@ -40,6 +41,7 @@ function _Mergetool_Toggle()
     mergetool:toggle()
 end
 
+vim.api.nvim_set_keymap("n", "<leader>te", "<cmd>lua _Yazi_Toggle()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>to", "<cmd>lua _Lazygit_Toggle()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>tm", "<cmd>lua _Mergetool_Toggle()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>tv", "<cmd>ToggleTerm direction=vertical<CR>", { noremap = true, silent = true })
