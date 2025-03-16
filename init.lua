fc1bb5be8cfe33971be54736e5a7526e013afc93
Matt McGinnis <mcginnisma90@gmail.com>
2025-03-15 21:27:28 -0400

    feature: File organization

--- init.lua
+++ init.lua
@@ -1,11 +1,2 @@
-require("core.colors")
-require("core.keymaps")
-require("core.options")
-require("core.plugins")
-require("core.plugin_config")
-
-vim.o.background = "dark" -- or "light" for light mode
-
-vim.cmd("colorscheme dracula")
-
-ColorMyScheme()
+require("config.lazy")
+require("config")
