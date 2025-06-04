9ab8896cacf68a03ca9dd91a16936a33fd9b6643
Matt McGinnis <mcginnisma90@gmail.com>
2025-06-04 08:48:11 -0400

    feature: Supermaven keymaps

--- lua/config/supermaven.lua
+++ lua/config/supermaven.lua
@@ -1,1 +1,7 @@
-require("supermaven-nvim").setup({})
+require("supermaven-nvim").setup({
+  keymaps = {
+    accept_suggestion = "<c-e>",
+    clear_suggestion = "<c-h>>",
+    accept_word = "<c-n>",
+  },
+})
