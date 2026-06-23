bfe8ac58b98116263ce35b9f67844691a6a169e8
Matt McGinnis <mcginnisma90@gmail.com>
2026-06-23 09:36:37 -0400

    chore: change movement

--- lua/config/movement.lua
+++ lua/config/movement.lua
@@ -1,4 +1,4 @@
 require('leap').opts.safe_labels = { 'f', 'n', 'u', 't' }
-vim.keymap.set({ 'n', 'x', 'o' }, 'f', '<Plug>(leap-forward)')
-vim.keymap.set({ 'n', 'x', 'o' }, 'F', '<Plug>(leap-backward)')
-vim.keymap.set({ 'n' }, '<leader>F', '<Plug>(leap-from-window)')
+vim.keymap.set({ 'n', 'x', 'o' }, 't', '<Plug>(leap-forward)')
+vim.keymap.set({ 'n', 'x', 'o' }, 'T', '<Plug>(leap-backward)')
+vim.keymap.set({ 'n' }, '<leader>T', '<Plug>(leap-from-window)')
